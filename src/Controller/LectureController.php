53ee625bd98544bc2b356c448b4e806e89a86a2d
Tomas Votruba <tomas.vot@gmail.com>
2018-07-23 00:51:34 +0200

    init lectures

---
+++ src/Controller/LectureController.php
@@ -0,0 +1,80 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Form\ProvisionFormType;
+use App\Request\ProvisionFormRequest;
+use OpenLecture\Provision\Data\PartnerData;
+use OpenLecture\Provision\Data\ProvisionData;
+use OpenLecture\Provision\ProvisionResolver;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class LectureController extends AbstractController
+{
+    /**
+     * @Route(path="/lectures/", name="lectures")
+     * See https://symfony.com/doc/current/quick_tour/the_controller.html#using-formats
+     */
+    public function default(): Response
+    {
+        return $this->render('lecture/default.twig');
+    }
+
+//    /**
+//     * @Route(path="/provision-result", methods={"POST"}, name="process_provision_form")
+//     */
+//    public function processProvisionForm(Request $request): Response
+//    {
+//        $provisionFormRequest = new ProvisionFormRequest();
+//
+//        $form = $this->createProvisionForm($provisionFormRequest);
+//        $form->handleRequest($request);
+//
+//        if ($form->isSubmitted() && $form->isValid()) {
+//            $provisionData = $this->createProvisionDataFromProvisionFormRequest($provisionFormRequest);
+//
+//            $this->provisionResolver->resolve($provisionData);
+//
+//            return $this->render('provision/result.twig', [
+//                'provisionData' => $provisionData,
+//            ]);
+//        }
+//
+//        return $this->render('provision/default.twig');
+//    }
+//
+//    /**
+//     * Call in Twig template as:
+//     * {{ render(controller('App\\Controller\\ProvisionController::componentProvisionForm()')) }}
+//     */
+//    public function componentProvisionForm(): Response
+//    {
+//        return $this->render('component/provisionForm.twig', [
+//            'form' => $this->createProvisionForm()->createView(),
+//        ]);
+//    }
+//
+//    /**
+//     * @param mixed|null $data
+//     */
+//    private function createProvisionForm($data = null): FormInterface
+//    {
+//        return $this->createForm(ProvisionFormType::class, $data, [
+//            'action' => $this->generateUrl('process_provision_form'),
+//        ]);
+//    }
+//
+//    private function createProvisionDataFromProvisionFormRequest(
+//        ProvisionFormRequest $provisionFormRequest
+//    ): ProvisionData {
+//        $partnerDatas[] = new PartnerData('Lector', 0.5, $provisionFormRequest->getOwnerExpenses());
+//        $partnerDatas[] = new PartnerData('Organizer', 0.25, $provisionFormRequest->getOwnerExpenses());
+//        $partnerDatas[] = new PartnerData('Owner', 0.25, $provisionFormRequest->getOwnerExpenses());
+//
+//        return new ProvisionData($provisionFormRequest->getIncomeAmount(), $partnerDatas);
+//    }
+}
