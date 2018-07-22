3c028630c1e1ce49b8b07ae5b4f93a58d8b4a4f9
Tomas Votruba <tomas.vot@gmail.com>
2018-07-22 18:49:40 +0200

    cleanup

--- src/Form/RegisterToLectureFormType.php
+++ src/Form/RegisterToLectureFormType.php
@@ -4,8 +4,6 @@ namespace App\Form;
 
 use App\Request\ProvisionFormRequest;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
